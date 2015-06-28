ef87699b2c7440292325d6b60a5728c1623cf225
474491418 <474491418@qq.com>
2015-06-28 14:38:51 +0800

    to change

---
+++ MyScala/src/edu/sust/test/ActorSendMessageAdd.scala
@@ -0,0 +1,41 @@
+package edu.sust.test
+
+import scala.actors.Actor._
+import scala.actors.TIMEOUT
+
+/**
+ * Created by envy15 on 2015/6/28 0028.
+ */
+class ActorSendMessageAdd {
+
+}
+
+object SendMessageStyle {
+
+  def main(args: Array[String]): Unit = {
+    val caller = self
+    val accumulator = actor {
+      var continue = true
+      var sum = 0
+      loopWhile(continue) {
+        reactWithin(500) {
+          case number: Int => sum += number
+          case TIMEOUT =>
+            continue = false
+            caller ! sum
+
+        }
+
+      }
+
+    }
+    accumulator ! 1
+    accumulator ! 2
+    accumulator ! 3
+    accumulator ! 4
+    receiveWithin(1000) {
+      case result => println("Total is " + result)
+
+    }
+
+  }
---
+++ MyScala/src/edu/sust/test/SelfTest.scala
@@ -0,0 +1,19 @@
+package edu.sust.test
+
+/**
+ * Created by envy15 on 2015/6/28 0028.
+ */
+class SelfTest {
+
+}
+
+//trait A extends B {
+//  this: B =>
+//}
+//
+//trait B extends A {
+//  this: A =>
+//}
+
+
+}
